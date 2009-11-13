97927d2afa3b0a09e3e6c37de550d300439732c9
Nickolay Schwarz <nickolay.schwarz@centro.net>
2009-11-13 17:22:43 -0600

    Added main hero animation

--- lib/player.rb
+++ lib/player.rb
@@ -3,43 +3,38 @@ class Player
   def initialize(window, x, y)
     @x, @y = x, y
     @map = window.map
-    @img = Gosu::Image.load_tiles(window, "resources/test2.png", 16,  16, false)
-    @xwalking = Gosu::Image.load_tiles(window, "resources/char_right_2.png", 16, 16, false)
+    right = Gosu::Image.load_tiles(window, "resources/char_right_2.png", 16, 16, false)
+    down = Gosu::Image.load_tiles(window, "resources/char_down_1.png", 16, 16, false)
+    left = Gosu::Image.load_tiles(window, "resources/char_left_1.png", 16, 16, false)
+    @animation_sprites = {:left  => left,
+                          :down  => down,
+                          :up    => down,
+                          :right => right}
     @img_index = 0
-    @facing = nil
+    @facing = :down
   end
   def draw(x, y)
-    # Flip vertically when facing to the left.
-    if @facing == :right then
-      offs_x = -8
-      factor = 1.0
-    else
-      offs_x = 8
-      factor = -1.0
-    end
-    @xwalking[@img_index].draw(@x - x + offs_x, @y - y, 0, factor, 1)
+    @animation_sprites[@facing][@img_index].draw(@x - x - 1, @y - y - 1 , 1, 1)
   end
 
   def update(direction)
+    @img_index += 1
     case direction
     when :left
-      @img_index += 1
-      5.times { @x-= 1 if would_fit?(-1, 0, 0, 0) && would_fit?(-1, 0, 0, 15) }
+      5.times { @x-= 1 if would_fit?(-1, 0, 0, 0) && would_fit?(-1, 0, 0, 14) }
       @facing = :left
     when :right
-      @img_index += 1
-      5.times { @x+= 1  if would_fit?(1, 0, 15, 0) && would_fit?(1, 0, 15, 15) }
+      5.times { @x+= 1  if would_fit?(1, 0, 14, 0) && would_fit?(1, 0, 14, 14) }
       @facing = :right
     when :up
-      5.times { @y-= 1 if would_fit?(0, -1, 15, 0) && would_fit?(0, -1, 0, 0) }
+      5.times { @y-= 1 if would_fit?(0, -1, 14, 0) && would_fit?(0, -1, 0, 0) }
       @facing = :up
     when :down
-      5.times { @y+= 1 if would_fit?(0, 1, 15, 15) && would_fit?(0, 1, 0, 15) }
+      5.times { @y+= 1 if would_fit?(0, 1, 14, 14) && would_fit?(0, 1, 0, 14) }
       @facing = :down
     else
       @img_index = 0
     end
-
     @img_index = 0 if @img_index == 4
   end
 
